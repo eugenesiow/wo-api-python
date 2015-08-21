9122615f64f6dc8e4129d404cb3e702800593aa7
Eugene Siow <kyo116@gmail.com>
2015-08-21 12:11:41 +0700

    UI Summer School commit

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # wo-api-python
 Web observatory API for python
 
-
-requirement:
-sudo apt-get install python-dev libffi-dev
+requirements:
+(sudo apt-get install python-dev libffi-dev)
+(python get-pip.py)
+sudo pip install requests
+sudo pip install socketIO_client
 
 
 if on ubuntu 12, remove the outdated openssl and install pyopenssl using pip:
--- example.py
+++ example.py
@@ -9,14 +9,14 @@ logging.captureWarnings(True)
 
 
 
-appid = "55590b16a7f6527a06ebe96d"
-appsec = "e4b72c8cb25d32ba" 
+appid = "55d6b06b4d6b5d1d21449967"
+appsec = "eac2b1a25ef936ce" 
 username = ""
 password = ""
 
 
 
-client = WO('webobservatory.soton.ac.uk', appid, appsec, username ,password )
+client = WO('ui.webobservatory.me', appid, appsec, username ,password )
 
 ##login after the client creation
 res = client.login()
@@ -25,37 +25,10 @@ if res:
 
 
 ##demo of the query using a string:
-err, res1 = client.query('52e19220bef627683c79c3a6','SELECT * WHERE {  ?subject rdf:type ?class} LIMIT 10')
+err, res1 = client.query('55d587a24d6b5d1d21449966','select * from iot.traffic_data limit 50;')
 if not err:
     print "\n\n"
     print "RESULT 1:"
     print res1
 else:
     print err
-
-
-##query using a dict:
-opt = {'collection':"abuse_entries_2014_12", 'query':'{}', 'limit':10,'skip':0}
-err, res2 = client.query('54eb0a1d590ea23530f24644', opt)
-if not err:
-    print "\n\n"
-    print "RESULT 2:"
-    print res2
-else:
-    print err
-##
-
-
-def callback(err, data,socket):
-        if not err:
-                print 'data:'+data
-#                socket.emit("stop")
-#                print "stopped"
-        else:
-            print err
-
-client.openStream('54eb13d7590ea23530f24645','logs',callback)
-#client.openStream('543e79000124b4b01c40612f','wikipedia_hose',callback)
-
-
-
